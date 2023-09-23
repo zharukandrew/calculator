1cd0ada8e4e1d4f3e62fb098570d039de2c996e0
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2023-09-23 15:06:58 +0400

    jhhh

--- script.js
+++ script.js
@@ -1,90 +1,59 @@
-let a ='';//first number
-let b = '';//second number
-let sign = '';//знак операции
-let finish = false;
+let result = document.querySelector(".calc-screen p");
+let buttonsBox = document.querySelector(".buttons");
+let r = document.querySelector('.equal')
 
-const digit = ['0','1','2','3','4','5','6','7','8','9','.'];
-const action =['-','+','X','/'];
+let nums = []
+let znac = []
 
-//экран 
-const out = document.querySelector('.calc-screen p');
+result.innerText = ''
 
-function clearAll () {
-    a ='';//first number
-    b = '';//second number
-    sign = '';//знак 
-    finish = false;
-    out.textContent = 0;
-}
+buttonsBox.addEventListener('click', (e)=>{
+    let kay = e.target.innerText
 
-document.querySelector('.ac').onclick = clearAll;
-document.querySelector('.buttons').onclick = (event) => {
-    // нажата не кнопка 
-    if(!event.target.classList.contains('btn')) return;
-    //нажата кнопка clearAll ac
-    if(event.target.classList.contains('ac')) return;
-
-    out.textContent = '';
-    //получаю нажатую кнопку
-    const key = event.target.textContent;
-
-    //если нажата 0-9 или .
-    
-    if(digit.includes(key)){
-        if(b ==='' && sign === ''){
-            a+=key;
-            
-            out.textContent = a;
-        }
-        else if (a!=='' && b!==''&& finish){
-            b = key;
-            finish = false ;
-            out.textContent = b;
-        }
-        else {
-            b += key;
-            out.textContent = b;
-        }
-        console.table(a , b , sign);
-        return;
+    if (Number(kay)) {
+        nums.push(kay)
+    } else {
+        znac.push(kay)
     }
 
-    //если нажата клавиша + - / *
-    if(action.includes(key)) {
-        sign = key;
-        out.textContent = sign;
-        console.log(a , b , sign);
-        return;
+    if (kay !== '==') {
+        result.innerText += kay
+    }
 
-        
-    }
-    //нажата =
-    if (key === '=') {
-        if(b === '') b = a;
-        switch (sign){
-            case "+":
-                a = (+a) + (+b);
-                break;
-            case "-":
-                a = (a) - (b);
-                break;
-            case "X":
-                a = (a) * (b);
-                break;
-            case "/":
-                if(b === '0') {
-                    out.textContent = 'Ошибка';
-                    a ='';
-                    b ='';
-                    sign = '';
-                    return;
-                }
-                a = (a) / (b);
-                break;
-        }
-        finish = true ;
-        out.textContent = a;
-        console.table(a , b , sign);
-    }
-};
+})
+
+r.addEventListener('click', ()=>{
+        result.innerText = ''
+        let a = nums[0]
+        let operand = znac[0]
+        let b = nums[1]
+
+        let res = 0
+
+        switch(operand) {
+            case '+': 
+              res = a + b
+              break
+          
+            case '-':
+              res = a - b
+              break
+
+            case '/':
+              res = a / b
+              break
+
+            case '*':
+              res = a * b
+              break
+          
+            default:
+              null
+              break
+          }
+
+        result.innerText = res
+        nums = []
+        znac = []
+})
 
